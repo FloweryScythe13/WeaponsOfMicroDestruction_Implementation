48562e72564afa045503cbba2dcb4b2c2d3cdddb
Eric Brichetto <Eric.Brichetto@toro.com>
2019-09-07 16:08:22 -0500

    added the csv file to the solution and cleaned up some artifacts from the cookiecutter template

--- WeaponsofMicroDestruction_Implementation/regression.py
+++ WeaponsofMicroDestruction_Implementation/regression.py
@@ -1,46 +1,9 @@
 '''
-This script perfoms the basic process for applying a machine learning
-algorithm to a dataset using Python libraries.
-
-The four steps are:
-   1. Download a dataset (using pandas)
-   2. Process the numeric data (using numpy)
-   3. Train and evaluate learners (using scikit-learn)
-   4. Plot and compare results (using matplotlib)
-
-
-The data is downloaded from URL, which is defined below. As is normal
-for machine learning problems, the nature of the source data affects
-the entire solution. When you change URL to refer to your own data, you
-will need to review the data processing steps to ensure they remain
-correct.
-
-============
-Example Data
-============
-The example is from http://mldata.org/repository/data/viewslug/stockvalues/
-It contains stock prices and the values of three indices for each day
-over a five year period. See the linked page for more details about
-this data set.
-
-This script uses regression learners to predict the stock price for
-the second half of this period based on the values of the indices. This
-is a naive approach, and a more robust method would use each prediction
-as an input for the next, and would predict relative rather than
-absolute values.
+This script is a Python implementation of the proof-of-concept data model for personality trait prediction by Dave Smith, from his 
+article "Weapons of Micro Destruction: How Our 'Likes' Hijacked Democracy" (https://towardsdatascience.com/weapons-of-micro-destruction-how-our-likes-hijacked-democracy-c9ab6fcd3d02), itself an attempt to reverse-engineer a POC of CA's model. It also 
+serves as an exercise for me in learning and understanding more about linear regression, especially L1 regularization with LASSO.
 '''
 
-# Remember to update the script for the new data when you change this URL
-URL = "http://mldata.org/repository/data/download/csv/stockvalues/"
-
-# This is the column of the sample data to predict.
-# Try changing it to other integers between 1 and 155.
-TARGET_COLUMN = 32
-
-# Uncomment this call when using matplotlib to generate images
-# rather than displaying interactive UI.
-#import matplotlib
-#matplotlib.use('Agg')
 
 import pandas as pd
 import numpy as np
@@ -58,22 +21,8 @@ def download_data():
     '''
     Downloads the data for this script into a pandas DataFrame.
     '''
-
-    # If your data is in an Excel file, install 'xlrd' and use
-    # pandas.read_excel instead of read_table
-    #from pandas import read_excel
-    #frame = read_excel(URL)
-
-    # If your data is in a private Azure blob, install 'azure-storage' and use
-    # BlockBlobService.get_blob_to_path() with read_table() or read_excel()
-    #from azure.storage.blob import BlockBlobService
-    #service = BlockBlobService(ACCOUNT_NAME, ACCOUNT_KEY)
-    #service.get_blob_to_path(container_name, blob_name, 'my_data.csv')
-    #frame = read_table('my_data.csv', ...
-
+   
     frame: pd.DataFrame = pd.read_csv("Data/WeaponsOfMicroDestructionRawData.csv");
-
-    # Return the entire frame
     return frame
 
 
@@ -121,7 +70,8 @@ def get_features_and_labels(frame):
     y_test_mean = np.average(y_test)
     y_test_std = np.std(y_test)
     y_train = (y_train - y_train_mean)/y_train_std
-    y_test = (y_test - y_train_mean)/y_train_std
+    y_test = (y_test - y_train_mean)/y_train_std #NOTE: this is what Smith did in his Excel calculations, but I do not understand the reasoning behind it.
+    # Why would you apply the mean & std from the training data instead of the test data when standardizing the test data labels?
     # Return the training and test sets
     return X_train, X_test, y_train, y_test
 
@@ -152,92 +102,10 @@ def evaluate_learner(X_train, X_test, y_train, y_test, alpha):
     r_2 = lasso.score(X_test, y_test)
     yield 'Lasso Model w/ Alpha = {0} ($R^2={1:.3f}$, MSE={2})'.format(alpha, r_2, mse), y_test, y_pred
 
-    
-
-
-# =====================================================================
-
-
-def plot(results):
-    '''
-    Create a plot comparing multiple learners.
-
-    `results` is a list of tuples containing:
-        (title, expected values, actual values)
-    
-    All the elements in results will be plotted.
-    '''
-
-    # Using subplots to display the results on the same X axis
-    fig, plts = plt.subplots(nrows=len(results), figsize=(8, 8))
-    fig.canvas.set_window_title('Predicting data from ' + URL)
-
-    # Show each element in the plots returned from plt.subplots()
-    for subplot, (title, y, y_pred) in zip(plts, results):
-        # Configure each subplot to have no tick marks
-        # (these are meaningless for the sample dataset)
-        subplot.set_xticklabels(())
-        subplot.set_yticklabels(())
-
-        # Label the vertical axis
-        subplot.set_ylabel('stock price')
-
-        # Set the title for the subplot
-        subplot.set_title(title)
-
-        # Plot the actual data and the prediction
-        subplot.plot(y, 'b', label='actual')
-        subplot.plot(y_pred, 'r', label='predicted')
-        
-        # Shade the area between the predicted and the actual values
-        subplot.fill_between(
-            # Generate X values [0, 1, 2, ..., len(y)-2, len(y)-1]
-            np.arange(0, len(y), 1),
-            y,
-            y_pred,
-            color='r',
-            alpha=0.2
-        )
-
-        # Mark the extent of the training data
-        subplot.axvline(len(y) // 2, linestyle='--', color='0', alpha=0.2)
-
-        # Include a legend in each subplot
-        subplot.legend()
-
-    # Let matplotlib handle the subplot layout
-    fig.tight_layout()
-
-    # ==================================
-    # Display the plot in interactive UI
-    plt.show()
-
-    # To save the plot to an image file, use savefig()
-    #plt.savefig('plot.png')
-
-    # Open the image file with the default image viewer
-    #import subprocess
-    #subprocess.Popen('plot.png', shell=True)
-
-    # To save the plot to an image in memory, use BytesIO and savefig()
-    # This can then be written to any stream-like object, such as a
-    # file or HTTP response.
-    #from io import BytesIO
-    #img_stream = BytesIO()
-    #plt.savefig(img_stream, fmt='png')
-    #img_bytes = img_stream.getvalue()
-    #print('Image is {} bytes - {!r}'.format(len(img_bytes), img_bytes[:8] + b'...'))
-
-    # Closing the figure allows matplotlib to release the memory used.
-    plt.close()
-
-
-# =====================================================================
-
 
 if __name__ == '__main__':
     # Download the data set from URL
-    print("Downloading data from {}".format(URL))
+    print("Downloading data from file")
     frame = download_data()
 
     # Process data into feature and label arrays
